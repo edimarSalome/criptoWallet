454b7f9113065d46564f78760d31659737decf45
edimarSalome <edimarjunior@outlook.com>
2017-12-20 11:18:35 -0200

    KickStart

---
+++ src/js/route-config.js
@@ -0,0 +1,23 @@
+criptoWallet.config(['$routeProvider',
+    function ($routeProvider) {
+
+        $routeProvider/*
+                .when('/agenda', {
+                    templateUrl: 'views/agenda_web/agenda_web.html',
+                    controller: 'agendaCtrl',
+                    controllerAs: '$ctrl'
+                })
+                .when('/login', {
+                    templateUrl: 'views/login/login.html',
+                    controller: 'loginCtrl',
+                    controllerAs: '$ctrl'
+                })*/
+                .when('/general', {
+                    templateUrl: 'app/index.html'
+                    //controller: 'generalCtrl',
+                    //controllerAs: '$ctrl'
+                })
+                .otherwise({
+                    redirectTo: '/general'
+                });
+    }]);
